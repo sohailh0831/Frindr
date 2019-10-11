ecb2c1406e5397b1312c10c8ece56364fd660f80
Joseph Busse <31547084+joebuss@users.noreply.github.com>
2019-10-10 22:25:23 -0400

    "completed" the algorithm UNTESTED

--- Backend/functions/matching.js
+++ Backend/functions/matching.js
@@ -24,37 +24,53 @@ export const getMatches = async (req, res) => {
         if (!req.body.email) {
           throw new Error("Need email")
         }
+        let compare;
+        if(!req.body.try){
+          compare = 5;
+        }else if (req.body.try < 6){
+          compare = 5-req.body.try;
+        } else {
+          throw new Error("No results")
+        }
         let results = await getProfileIntern(req);
         if (results.error == true) {
           return res.status('400').send(results);
         }
         const userProfile = results.message[0];
-
-        results = await getProfilesStore(req);
+        
+        results = await getProfilesStore(req, userProfile.location);
         if (results.error == true) {
             return res.status('400').send(results);
           }
         console.log(results);
-        
-
-          return;
-
-
-
-
-
-
-
+        let list = [];
+        let userInterests = userProfile.interests;
+        for (var i = 0; i < results.message.length; i++){
+          let current = results.message[i];
+          interests = JSON.parse(userInterests);
+          let keys = Object.keys(interests);
+          let count = 0;
+          for(var j = 0; j < keys.length; j++){
+            let currKey = keys[i];
+            if(current[currKey] === userInterests[currKey]){
+              count++;
+            }
+          }
+          if (count >= compare){
+            list.push(current);
+          }
+        }
+        return res.status('200').send(list);
       } catch (error) {
         return res.status('400').send({ error: true, message: error.stack });
       }
 };
 
-function getProfilesStore(req){
+function getProfilesStore(req, location){
     return new Promise(resolve => {
         try {
           let con = mysql.createConnection(dbInfo);
-          con.query(`SELECT * FROM profile;`, (error, results, fields) => {
+          con.query(`SELECT * FROM profile where location = ${location};`, (error, results, fields) => {
             if (error) {
               console.log(error.stack);
               con.end();
@@ -77,5 +93,3 @@ function getProfilesStore(req){
         }
       });
 }
-
-
