06567f23ae0860f9e30ad24a6ff3083009148dd0
clabianc <cjlabianca99@gmail.com>
2019-10-19 20:04:12 -0400

    More tests for login page written

--- Backend/selenium/test.js
+++ Backend/selenium/test.js
@@ -2,7 +2,7 @@ require('chromedriver');
 const assert = require('assert');
 const { Builder, Key, By, until } = require('selenium-webdriver');
 
-describe('Basic checkup', function () {
+describe('Login page', function () {
    let driver;
    before(async function () {
       driver = await new Builder().forBrowser('chrome').build();
@@ -19,14 +19,34 @@ describe('Basic checkup', function () {
       assert( await driver.wait(until.elementLocated(By.className('m-login__logo')), 10000) );
    });
 
-   it('Check login fields exist', async function () {
+   it('Check username field exists', async function () {
       await driver.get('https://frindr.tk');
-      assert( await driver.wait(until.elementLocated(By.className('form-control m-input')), 10000) );
+      assert( await driver.wait(until.elementLocated(By.name('username')), 10000) );
    });
 
-   it('Check no account message exists', async function () {
+   it('Check password field exists', async function () {
       await driver.get('https://frindr.tk');
-      assert( await driver.wait(until.elementLocated(By.className('m-login__account-msg')), 10000) );
+      assert( await driver.wait(until.elementLocated(By.name('password')), 10000) );
+   });
+
+   it('Check sign in button exists', async function () {
+      await driver.get('https://frindr.tk');
+      assert( await driver.wait(until.elementLocated(By.id('m_login_signin_submit')), 10000) );
+   });
+
+   it('Verify error comes when sign in clicked with no input', async function () {
+      await driver.get('https://frindr.tk');
+      await driver.findElement(By.id('m_login_signin_submit')).click();
+      assert( await driver.wait(until.elementLocated(By.className('m-alert')), 10000) );
+   });
+
+   it('Verify move to dashboard after correct login', async function () {
+      await driver.get('https://frindr.tk');
+      await driver.findElement(By.name('username')).sendKeys('jj@legitemail.com');
+      await driver.findElement(By.name('password')).sendKeys('jimjimpass');
+      await driver.findElement(By.id('m_login_signin_submit')).click();
+      let currURL = await driver.getCurrentUrl();
+      assert.equal(currURL, 'https://frindr.tk/dashboard');
    });
 
    // close the browser after running tests
